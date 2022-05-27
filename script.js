b10db0869d32b1b5528057db879c2b52c0054177
Sviatoslav <43699040+kitaiv@users.noreply.github.com>
2022-05-27 11:01:45 +0300

    asd

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ document.addEventListener("DOMContentLoaded", () => {
           const { aspectRatio, frameRate, height, width, resizeMode, facingMode } = el.getCapabilities();
           console.log(el.getCapabilities())
           
-          p.innerHTML = `deviceId: ${deviceId} ${'\r\n'}<br/>`
-          p.innerHTML += `kind: ${kind}<br/>`
-          p.innerHTML += `label: ${label}<br/>`
-          p.innerHTML += `groupId: ${groupId}<br/>`
-          p.innerHTML += `aspectRatio: max: ${aspectRatio.max} min: ${aspectRatio.min}<br/>`
-          p.innerHTML += `facingMode: ${facingMode.map(el => el)}<br/>`
-          p.innerHTML += `frameRate: max: ${frameRate.max} min: ${frameRate.min}<br/>`
-          p.innerHTML += `height: max: ${height.max} min: ${height.min}<br/>`
-          p.innerHTML += `resizeMode: ${resizeMode.map(el => el)}<br/>`
-          p.innerHTML += `width: max: ${width.max} min: ${width.min}<br/>`
+          p.innerHTML = `deviceId: ${deviceId ?? '-'} ${'\r\n'}<br/>`
+          p.innerHTML += `kind: ${kind ?? '-'}<br/>`
+          p.innerHTML += `label: ${label ?? '-'}<br/>`
+          p.innerHTML += `groupId: ${groupId ?? '-'}<br/>`
+          p.innerHTML += `aspectRatio: max: ${aspectRatio.max ?? '-'} min: ${aspectRatio.min ?? '-'}<br/>`
+          p.innerHTML += `facingMode: ${facingMode.map(el => el ?? '-')}<br/>`
+          p.innerHTML += `frameRate: max: ${frameRate.max ?? '-'} min: ${frameRate.min ?? '-'}<br/>`
+          p.innerHTML += `height: max: ${height.max ?? '-'} min: ${height.min ?? '-'}<br/>`
+          p.innerHTML += `resizeMode: ${resizeMode.map(el => el ?? '-')}<br/>`
+          p.innerHTML += `width: max: ${width.max ?? '-'} min: ${width.min ?? '-'}<br/>`
 
           document.body.appendChild(p);
         })
