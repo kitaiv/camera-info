335477e82f83056b1b172e2006af2d8f8adcb279
kitaiv <svlawd@gmail.com>
2022-05-28 18:01:13 +0300

    convert to array

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
+function getCameraData() {
   let contraints = {
-    video: {facingMode: "user"}
-  }
-  if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+    video: { facingMode: "user" },
+  };
+  if (
+    "mediaDevices" in navigator &&
+    "getUserMedia" in navigator.mediaDevices
+  ) {
     async function getVideoMedia(contraints) {
       let stream = null;
+      let data = [];
 
       try {
         stream = await navigator.mediaDevices.getUserMedia(contraints);
-        const media = (await navigator.mediaDevices.enumerateDevices())
-        const mediaVideoInputs = media && media.map(el => el.kind === "videoinput" && el || null).filter(i => i ?? false)
-        console.log('mediaVideoInputs >>> ', mediaVideoInputs)
-        mediaVideoInputs.forEach(el => {
-          const p = document.createElement('p');
-          const {deviceId, kind, label, groupId} = el;
-          const { aspectRatio, frameRate, height, width, resizeMode, facingMode } = el.getCapabilities();
-          console.log(el.getCapabilities())
-          
-          p.innerHTML = `deviceId: ${deviceId} ${'\r\n'}<br/>`
-          p.innerHTML += `kind: ${kind}<br/>`
-          p.innerHTML += `label: ${label}<br/>`
-          p.innerHTML += `groupId: ${groupId}<br/>`
-          p.innerHTML += `aspectRatio: max: ${aspectRatio.max} min: ${aspectRatio.min}<br/>`
-          p.innerHTML += `facingMode: ${facingMode.map(el => el)}<br/>`
-          p.innerHTML += `frameRate: max: ${frameRate.max} min: ${frameRate.min}<br/>`
-          p.innerHTML += `height: max: ${height.max} min: ${height.min}<br/>`
-          p.innerHTML += `resizeMode: ${resizeMode.map(el => el)}<br/>`
-          p.innerHTML += `width: max: ${width.max} min: ${width.min}<br/>`
+        const media = await navigator.mediaDevices.enumerateDevices();
+        const mediaVideoInputs =
+          media &&
+          media
+            .map(el => (el.kind === "videoinput" && el) || null)
+            .filter(i => i ?? false);
+        const coreVideoInput =
+          mediaVideoInputs && mediaVideoInputs.length && mediaVideoInputs[0];
+        if (coreVideoInput) {
+          const {
+            aspectRatio,
+            frameRate,
+            height,
+            width,
+            resizeMode,
+            facingMode,
+            deviceId,
+          } = coreVideoInput.getCapabilities();
 
-          document.body.appendChild(p);
-        })
+          const { kind, label } = coreVideoInput;
+
+          data.push(
+            width &&
+              height &&
+              width.max.toString() + "x" + height.max.toString(),
+            aspectRatio && aspectRatio.max,
+            facingMode &&
+              facingMode.length &&
+              facingMode.map((el) => el.toString()).join("/"),
+            resizeMode &&
+              resizeMode.length &&
+              resizeMode.map((el) => el.toString()).join("/"),
+            deviceId && deviceId,
+            frameRate && frameRate.max,
+            kind && kind,
+            label && label
+          );
+          console.log(data)
+          return data;
+        }
+        return [];
       } catch (err) {
         console.error(err);
       }
     }
     getVideoMedia(contraints);
   }
-
-});
+  return new Error("Not supported");
+}
+getCameraData();
